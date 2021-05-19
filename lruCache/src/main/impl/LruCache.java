4929714fac755938c551e19b732a428553f09cb9
Vitalik <govital3@gmail.com>
2021-05-19 19:25:25 +0300

    implemented cache setter and getter

--- lruCache/src/main/impl/LruCache.java
+++ lruCache/src/main/impl/LruCache.java
@@ -5,57 +5,115 @@ import java.util.Map;
 
 class LruCache {
 
-    private Integer qLimit;
-    private boolean limitReached;
+    private final Integer capacity;
     private Integer tailId;
     private Integer headId;
     private Map<Integer, CacheNode> dataMap;
 
-    public LruCache(Integer qLimit) {
-        this.qLimit = qLimit;
-        this.limitReached = false;
+    public LruCache(Integer capacity) {
+        this.capacity = capacity;
         this.dataMap = new HashMap<>();
     }
 
     public String get(Integer id) {
-        //manage queue
-        // * take id out of its place in line.
-        // * insert id to tail
-        // * if queue limit reached: remove queue head ( from map also )
-        //
         this.nodeMoveToTail(id);
         return this.dataMap.get(id).getData();
-
     }
 
-
     public void set(Integer id, String val) {
+        updateOldTail(id);
+        updateNewTail(id, val);
+        while(dataMap.size() > capacity){
+            trimHead();
+        }
+    }
 
-        CacheNode newTail = new CacheNode(val, this.tailId);
+    public void print() {
+        String sb = getPrintableCacheOrder();
+        System.out.println(sb);
+    }
 
+    private String getPrintableCacheOrder() {
+        CacheNode currentNode = dataMap.get(headId);
+        Integer nextIdToPrint = headId;
+        StringBuilder sb = new StringBuilder();
+        sb.append("HEAD-->");
+        while(currentNode != null){
+            sb.append(nextIdToPrint.toString() + "-->");
+            nextIdToPrint = currentNode.getTo();
+            currentNode = dataMap.get(currentNode.getTo());
+        }
+        sb.append("TAIL");
+        return sb.toString();
+    }
 
+    private void updateOldTail(Integer newTailId) {
+        if (tailId == null) {
+            headId = newTailId;
+            return;
+        }
+        CacheNode oldTail = dataMap.get(tailId);
+        oldTail.setTo(newTailId);
+        dataMap.put(tailId, oldTail);
+    }
 
+    private void updateNewTail(Integer id, String val) {
+        CacheNode newTail = new CacheNode(val, tailId);
+        dataMap.put(id, newTail);
+        tailId = id;
+    }
 
+    private void trimHead() {
+        CacheNode headNode = dataMap.get(headId);
+        dataMap.remove(headId);
+        headId = headNode.getTo();
+
+        CacheNode newHeadNode = dataMap.get(headId);
+        newHeadNode.setFrom(null);
+        dataMap.put(headId, newHeadNode);
     }
 
     private void nodeMoveToTail(Integer id){
-        CacheNode theNode = this.dataMap.get(id);
+        CacheNode currentNode = this.dataMap.get(id);
+        this.previousNodeUpdate(currentNode);
+        this.nextNodeUpdate(currentNode, id);
+        this.currentNodeUpdateAsTail(currentNode, id);
+    }
 
-        if (theNode.getFrom() == null) {
-            this.headId = theNode.getTo();
+    private void previousNodeUpdate(CacheNode currentNode){
+        if (currentNode.getFrom() == null) {
+            this.headId = currentNode.getTo();
         }else {
-            CacheNode fromNode = this.dataMap.get(theNode.getFrom());
-            fromNode.setTo(theNode.getTo());
+            this.updateExistingPreviousNode(currentNode);
         }
+    }
 
-        CacheNode toNode = this.dataMap.get(theNode.getTo());
-        toNode.setFrom(theNode.getFrom());
+    private void updateExistingPreviousNode(CacheNode currentNode){
+        CacheNode fromNode = this.dataMap.get(currentNode.getFrom());
+        fromNode.setTo(currentNode.getTo());
+        this.dataMap.put(currentNode.getFrom(), fromNode);
+    }
 
-        theNode.setTo(null);
-        theNode.setFrom(this.tailId);
+    private void nextNodeUpdate(CacheNode currentNode, Integer currentNodeId){
+        CacheNode toNode = this.dataMap.get(currentNode.getTo());
+        if (toNode == null) {
+            return;
+        }
+        toNode.setFrom(currentNode.getFrom());
+        if (toNode.getFrom() == null) {
+            headId = currentNode.getTo();
+        }
+        if (toNode.getTo() == null) {
+            toNode.setTo(currentNodeId);
+        }
+        this.dataMap.put(currentNode.getTo(), toNode);
+    }
 
+    private void currentNodeUpdateAsTail(CacheNode currentNode, Integer id){
+        currentNode.setTo(null);
+        currentNode.setFrom(this.tailId);
+        this.dataMap.put(id, currentNode);
         this.tailId = id;
-
     }
 
 }
--- lruCache/src/main/impl/Main.java
+++ lruCache/src/main/impl/Main.java
@@ -10,6 +10,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        LruCache cache = new LruCache(3);
+        cache.set(3, "3");
+        cache.set(2, "2");
+        cache.get(3);
+        cache.set(1, "1");
+        cache.set(4, "4");
+        cache.print();
     }
 
 }
