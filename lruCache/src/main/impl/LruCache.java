75b7109a0e97cb1a8c2a38d869f663eca9916efd
Vitalik <govital3@gmail.com>
2021-05-24 20:30:42 +0300

    iadded more robust test cases

--- lruCache/src/main/impl/LruCache.java
+++ lruCache/src/main/impl/LruCache.java
@@ -15,17 +15,86 @@ class LruCache {
         this.dataMap = new HashMap<>();
     }
 
-    public String get(Integer id) {
+    public void get(Integer id) {
+
+        if (this.dataMap.get(id) == null) {
+            return;
+        }
+        if (id == headId && id == tailId) {
+            return;
+        }
+        if (id == tailId) {
+            return;
+        }
         this.nodeMoveToTail(id);
-        return this.dataMap.get(id).getData();
     }
 
     public void set(Integer id, String val) {
+        handleExistingIdSet(id);
         updateOldTail(id);
         updateNewTail(id, val);
         while(dataMap.size() > capacity){
             trimHead();
         }
+    }
+
+    private void handleExistingIdSet(Integer id) {
+        CacheNode existingNode = dataMap.get(id);
+
+        /**
+         * setting when map is empty
+         */
+        if (dataMap.size() == 0) {
+            return;
+        }
+
+        /**
+         * setting when node not exist already in map
+         */
+        if (dataMap.get(id) == null) {
+            return;
+        }
+
+
+        /**
+         * setting when only single node and its same
+         */
+        if (id == headId && id == tailId) {
+            return;
+        }
+
+        /**
+         * setting when node exist and its tail
+         */
+        if (id == tailId) {
+            return;
+        }
+
+        CacheNode toNode;
+        CacheNode fromNode;
+
+        /**
+         * setting when id already exists and its the HEAD node.
+         */
+        if (id == headId) {
+            toNode = dataMap.get(existingNode.getTo());
+            toNode.setFrom(null);
+            headId = existingNode.getTo();
+            dataMap.put(existingNode.getTo(), toNode);
+            return;
+        }
+
+        /**
+         * setting when id already exists and its one of middle nodes.
+         */
+        if (id != tailId && dataMap.get(id) != null) {
+            toNode = dataMap.get(existingNode.getTo());
+            fromNode = dataMap.get(existingNode.getFrom());
+            fromNode.setTo(existingNode.getTo());
+            toNode.setFrom(existingNode.getFrom());
+            return;
+        }
+
     }
 
     public void print() {
@@ -37,7 +106,7 @@ class LruCache {
         CacheNode currentNode = dataMap.get(headId);
         Integer nextIdToPrint = headId;
         StringBuilder sb = new StringBuilder();
-        sb.append("HEAD-->");
+        sb.append("REAL     OUTPUT: HEAD-->");
         while(currentNode != null){
             sb.append(nextIdToPrint.toString() + "-->");
             nextIdToPrint = currentNode.getTo();
@@ -81,9 +150,7 @@ class LruCache {
     }
 
     private void previousNodeUpdate(CacheNode currentNode){
-        if (currentNode.getFrom() == null) {
-            this.headId = currentNode.getTo();
-        }else {
+        if (currentNode.getFrom() != null) {
             this.updateExistingPreviousNode(currentNode);
         }
     }
@@ -96,24 +163,24 @@ class LruCache {
 
     private void nextNodeUpdate(CacheNode currentNode, Integer currentNodeId){
         CacheNode toNode = this.dataMap.get(currentNode.getTo());
-        if (toNode == null) {
-            return;
-        }
-        toNode.setFrom(currentNode.getFrom());
-        if (toNode.getFrom() == null) {
+        if (currentNodeId == headId) {
+            toNode.setFrom(null);
             headId = currentNode.getTo();
-        }
-        if (toNode.getTo() == null) {
-            toNode.setTo(currentNodeId);
+        }else {
+            toNode.setFrom(currentNode.getFrom());
         }
         this.dataMap.put(currentNode.getTo(), toNode);
     }
 
     private void currentNodeUpdateAsTail(CacheNode currentNode, Integer id){
+        CacheNode oldTail = this.dataMap.get(tailId);
+        oldTail.setTo(id);
+        Integer oldTailId = tailId;
         currentNode.setTo(null);
         currentNode.setFrom(this.tailId);
-        this.dataMap.put(id, currentNode);
         this.tailId = id;
+        this.dataMap.put(id, currentNode);
+        this.dataMap.put(oldTailId, oldTail);
     }
 
 }
--- lruCache/src/main/impl/Main.java
+++ lruCache/src/main/impl/Main.java
@@ -7,14 +7,56 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
+    static LruCache cache;
 
     public static void main(String[] args) throws IOException {
+        Main.testOne();
+        Main.testTwo();
+        Main.testThree();
+    }
 
-        LruCache cache = new LruCache(3);
+    private static void testOne() {
+        System.out.println("TEST 1");
+        System.out.println("EXPECTED OUTPUT: HEAD-->3-->1-->4-->TAIL");
+        cache = new LruCache(3);
         cache.set(3, "3");
         cache.set(2, "2");
         cache.get(3);
         cache.set(1, "1");
+        cache.set(4, "4");
+        cache.print();
+    }
+
+    private static void testTwo() {
+        System.out.println("TEST 2");
+        System.out.println("EXPECTED OUTPUT: HEAD-->3-->1-->4-->TAIL");
+        cache = new LruCache(3);
+        cache.get(3);
+        cache.get(3);
+        cache.set(3, "3");
+        cache.set(2, "2");
+        cache.get(3);
+        cache.set(1, "1");
+        cache.set(4, "4");
+        cache.print();
+    }
+
+    private static void testThree() {
+        System.out.println("TEST 3");
+        System.out.println("EXPECTED OUTPUT: HEAD-->3-->1-->4-->TAIL");
+        cache = new LruCache(3);
+        cache.get(3);
+        cache.get(3);
+        cache.set(3, "3");
+        cache.set(3, "3");
+        cache.set(3, "3");
+        cache.set(3, "3");
+        cache.set(2, "2");
+        cache.get(3);
+        cache.set(1, "1");
+        cache.set(4, "4");
+        cache.set(4, "4");
+        cache.set(4, "4");
         cache.set(4, "4");
         cache.print();
     }
