b9379446e24f58a933ce07cbc129ca1e61d7834d
Vitalik G <govital3@gmail.com>
2022-12-12 13:51:51 +0200

    Create ListNode.java

---
+++ AddTwoNumbers/ListNode.java
@@ -0,0 +1,23 @@
+package org.example.AddTwoNumbers;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+  /**Definition for singly-linked list. */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
