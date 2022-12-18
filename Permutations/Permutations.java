e78c84c601950347039b3f154ce439de87becabc
Vitalik G <govital3@gmail.com>
2022-12-18 09:24:48 +0200

    Update Permutations.java

--- Permutations/Permutations.java
+++ Permutations/Permutations.java
@@ -14,50 +14,37 @@ public class Permutations {
         if (nums.length == 0 || nums == null) {
             return Collections.emptyList();
         }
-        List<Node> current = new ArrayList();
+
+        List<Map<NodeKeys, List<Integer>>> next = new ArrayList();
+        List<List<Integer>> paths = new ArrayList<>();
         /**populate root nodes*/
         for (int i = 0; i < nums.length;i++) {
-            Node node = new Node();
-            node.path = new ArrayList<>();
-            node.path.add(nums[i]);
-            node.remaining = IntStream.of(nums).boxed().collect(Collectors.toList());
-            node.remaining.remove(node.remaining.get(i));
-            current.add(node);
-        }
-
-        /**create pormutation trees*/
-        List<Node> next = new ArrayList();
-        List<List<Integer>> paths = new ArrayList<>();
-        while (!current.isEmpty()) {
-            Node cn = current.get(0);
-            current.remove(0);
-            if (cn.remaining == null || cn.remaining.isEmpty()) {
-                paths.add(cn.path);
-                continue;
-            }
-
-            for (Integer n: cn.remaining) {
-                Node nn = new Node();
-                nn.remaining = new ArrayList<>(cn.remaining);
-                nn.remaining.remove(n);
-                nn.path = new ArrayList<>(cn.path);
-                nn.path.add(n);
-                next.add(nn);
-            }
-            if (current.isEmpty() && !next.isEmpty()) {
-                current = new ArrayList(next);
-                next = new ArrayList();
+            Map<NodeKeys, List<Integer>> node = new HashMap();
+            node.put(NodeKeys.PATH, new ArrayList(Arrays.asList(nums[i])));
+            node.put(NodeKeys.REMAINING, new ArrayList(IntStream.of(nums).boxed().collect(Collectors.toList())));
+            node.get(NodeKeys.REMAINING).remove(i);
+            next.add(node);
+            /**create pormutation trees*/
+            while (!next.isEmpty()) {
+                Map<NodeKeys, List<Integer>> cn = next.get(0);
+                next.remove(0);
+                /**no remaining means its leafe node - so add path to paths*/
+                if (cn.get(NodeKeys.REMAINING) == null || cn.get(NodeKeys.REMAINING).isEmpty()) {
+                    paths.add(cn.get(NodeKeys.PATH));
+                    continue;
+                }
+                for (Integer n: cn.get(NodeKeys.REMAINING)) {
+                    Map<NodeKeys, List<Integer>> nn = new HashMap<>();
+                    nn.put(NodeKeys.REMAINING, new ArrayList<>(cn.get(NodeKeys.REMAINING)));
+                    nn.get(NodeKeys.REMAINING).remove(n);
+                    nn.put(NodeKeys.PATH, new ArrayList<>(cn.get(NodeKeys.PATH)));
+                    nn.get(NodeKeys.PATH).add(n);
+                    next.add(nn);
+                }
             }
         }
         return paths;
-
     }
 
-
-
-    static class Node {
-        List<Integer> remaining;
-        List<Integer> path;
-    }
 
 }
