88f9e95bbb9c39218914c3ed181d6e54b79e899e
Vitalik G <govital3@gmail.com>
2023-01-23 16:01:31 +0200

    Update 56MergeIntervals.java

--- Numbers/56MergeIntervals.java
+++ Numbers/56MergeIntervals.java
@@ -4,6 +4,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] unMerged = new int[5][2];
+        unMerged[0] = new int[2];
+        unMerged[0][0]= 1;unMerged[0][1]= 4;
+        unMerged[1] = new int[2];
+        unMerged[1][0]= 0;unMerged[1][1]= 1;
+        unMerged[2] = new int[2];
+        unMerged[2][0]= 6;unMerged[2][1]= 7;
+        unMerged[3] = new int[2];
+        unMerged[3][0]= 8;unMerged[3][1]= 9;
+        unMerged[4] = new int[2];
+        unMerged[4][0]= 1;unMerged[4][1]= 10;
+        int[][] merged = solution.merge(unMerged);
+        System.out.println(Arrays.deepToString(merged));
+    }
+}
+*/
+
 public class Solution {
 
     public int[][] merge(int[][] intervals) {
